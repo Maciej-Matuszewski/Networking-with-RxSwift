76b7224fece3cd27e4fad4d0a64c4acb594696f4
Michael Tango <csqingyang@foxmail.com>
2019-11-24 13:16:05 +0800

    Refactor send func to more testable, having issue on func testSendRequests(), please have review on pr

--- Networking with RxSwift/APIClient.swift
+++ Networking with RxSwift/APIClient.swift
@@ -2,12 +2,18 @@ import Foundation
 import RxSwift
 
 class APIClient {
+    
+    private let engine: NetworkEngine
     private let baseURL = URL(string: "http://universities.hipolabs.com/")!
 
+    init(engine: NetworkEngine = URLSession.shared) {
+        self.engine = engine
+    }
+    
     func send<T: Codable>(apiRequest: APIRequest) -> Observable<T> {
         return Observable<T>.create { [unowned self] observer in
             let request = apiRequest.request(with: self.baseURL)
-            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            let task = self.engine.createRequest(for: request, completionHandler: { (data, response, error) in
                 do {
                     let model: T = try JSONDecoder().decode(T.self, from: data ?? Data())
                     observer.onNext(model)
@@ -15,7 +21,7 @@ class APIClient {
                     observer.onError(error)
                 }
                 observer.onCompleted()
-            }
+            })
             task.resume()
             
             return Disposables.create {
